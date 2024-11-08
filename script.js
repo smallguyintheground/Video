1624813dc2230207764aabb872ca5837b85f4fad
smallguyintheground <imsmallguyintheground@gmail.com>
2024-11-08 16:57:35 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to change the message when the button is clicked
+function changeMessage() {
+    document.querySelector('h1').textContent = "You clicked the button!";
+    document.querySelector('p').textContent = "Thanks for interacting with me!";
+}
